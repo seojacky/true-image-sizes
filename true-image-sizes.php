ff23816861d7eed3055225d471262a6324c4aabc
Eugen <pustojack@list.ru>
2021-12-07 16:49:26 +0200

    Update true-image-sizes.php

--- true-image-sizes.php
+++ true-image-sizes.php
@@ -2,7 +2,7 @@
 /*
  * Plugin name: True Image & True Sizes
  * Description: Плагин для генерации набора изображений в соотношениях сторон 1:1, 4:3, 16:9.
- * Version: 0.2
+ * Version: 0.3
  * Author: @big_jacky 
  * Author URI: https://t.me/big_jacky
  * Plugin URI: https://github.com/seojacky/true-image-sizes
@@ -12,10 +12,16 @@
 /* Exit if accessed directly */
 if ( ! defined( 'ABSPATH' ) ) {	return;}
 
+// ВАЖНЫЕ ПЕРЕМЕННЫЕ!
+define('TITS__PLUGIN_URL', trailingslashit(plugin_dir_url(__FILE__))); // Урл папки плагина. Для подключения картинок и скриптов из внутренних папок (со слешем в конце)
+define('TITS__PLUGIN_DIR', trailingslashit(plugin_dir_path(__FILE__))); // Системный путь до папки плагина, нужен для include_once
+define('TITS_FILE', __FILE__); // Путь до главного (то есть до этого) файла плагина
 
-// Тоже самое можно сделать в плагине Simple Image Sizes
-// рекомендую его к установке, так как тогда можно на каждом отдельном изображении перегенерировать размеры
-// пример https://i.imgur.com/UcJt9DU.jpg
+define('TITS_SLUG', 'true-image-sizes');
+define('TITS_DIR',  __DIR__);
+
+require_once DBSC_DIR . '/functions.php';
+
 add_image_size( '4x3_640x480', '640', '480', true );
 add_image_size( '4x3_920x690', '920', '690', true );
 add_image_size( '4x3_1280x960', '1280', '960', true );
