d1b20520dd6230e5a18d5a7f16cb7ad204b103d2
Eugen <pustojack@list.ru>
2021-12-07 16:55:01 +0200

    Create setting-page.php

---
+++ setting-page.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ *****************************************************************
+  Создаем страницу настроек плагина
+****************************************************************
+*/
+/* Добавляем  с проверкой типовой пункт меню  WP Booster */
+add_action('admin_menu', 'tits_creat_admin_page', 8, 0);
+
+function tits_creat_admin_page(){	
+	global $admin_page_hooks;	
+	if (isset($admin_page_hooks['wp-booster'])  ) {
+		return;
+	}
+	
+add_menu_page(
+        esc_html__('WP Booster', TITS_SLUG),
+        esc_html_x('WP Booster', 'Menu item', TITS_SLUG),
+		'manage_options',
+		'wp-booster',
+		'options_page_tits_output',
+		'dashicons-backup',
+		92.3 
+            );
+}	
+
+add_action('admin_head', function(){
+  	echo '<style>
+    .toplevel_page_wp-booster li.wp-first-item {
+    display: none;}
+  </style>';
+});
+
+	add_action('admin_menu', function(){
+	$submenu = add_submenu_page(
+	'wp-booster',
+	'Настройки плагина True Image & True Sizes',
+	'True Image & True Sizes',
+	'manage_options',
+	'tits_slug',
+	'options_page_tits_output'
+	);
+
+}, 99 );
